480e2e19f1e883b924ae97136030cc8dad653e08
erenirmak <irmak.eren@gmail.com>
2023-12-16 00:44:41 +0300

    Update FlaskDocker.py

--- FlaskDocker.py
+++ FlaskDocker.py
@@ -1,26 +1,34 @@
 
 from flask import Flask, render_template, send_file
+from sqlalchemy import create_engine
 import pandas as pd
 from datetime import datetime
 import os
 
-from sqlalchemy import create_engine
+# Connection String & Database Engine
 oracle_connection_string = f"{database}+{connector}://{user}:{password}@{host}:{port}/{SID}"
 oracle_engine = create_engine(oracle_connection_string)
 
-tarih = None
+# Variables
+today_date = None
 df = None
-    
+
+# Flask App
 app = Flask(__name__)
 
+# /download route:
 @app.route('/download')
 def download():
-    global tarih
+    global today_date
     global df
-    tarih = datetime.today().strftime('%d-%m-%Y')
+    # Get today date:
+    today_date = datetime.today().strftime('%d-%m-%Y')
+
+    # Save location & File name
     path = "./ReportLog/"
-    file = f"gunluk_rapor_{tarih}.csv"
-    
+    file = f"daily_report_{today_date}.csv"
+
+    # File creation
     if os.path.isdir(path):
         os.chdir(path)
         df.to_csv(file, index=False)
@@ -28,19 +36,26 @@ def download():
         os.mkdir(path)
         os.chdir(path)
         df.to_csv(file, index=False)
-        
+    
+    os.chdir("..")
+    # Download file
     return send_file(path+file, as_attachment=True)
 
+# index route:
 @app.route("/")
 def rapor():
     global df
+    # Connect to the database
     with oracle_engine.connect() as oracle_connection:
+        # Query construction
         tablename = "TABLE NAME"
         query = f"""SELECT COL1, SUM(COL2) AMOUNT FROM {tablename}
                     WHERE DATECOLUMN > TRUNC(CURRENT_DATE) - 2 AND
                     DATECOLUMN < TRUNC(CURRENT_DATE)
                     GROUP BY COLUMN"""
+        # Load query result as Pandas DataFrame
         df = pd.read_sql(query, oracle_connection)
+        
     # Convert the dataframe to HTML
     table = df.to_html(index=False)
     
